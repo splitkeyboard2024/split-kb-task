476709e3dffbf2633b13d9f8aa1e8100fe209f71
Kamil Zaremba <kamz190@gmail.com>
2024-04-10 23:51:37 +0200

    fix instruction

--- lines.py
+++ lines.py
@@ -1,5 +1,5 @@
-# Napisz funkcję, która wypisuje tekst (podany jako argument) oddzielając każde słowo przecinkiem i wypisując maksymalnie 5 słów w rzędzie.
-# UWAGA! Przypadki testowe mogą zawierać znaki ?!, znaki nowej linii \n i spacje na początku i końcu tekstu.
+# Napisz funkcję, która zwraca tekst (podany jako argument) oddzielając każde słowo przecinkiem i wypisując maksymalnie 5 słów w rzędzie.
+# UWAGA! Przypadki testowe mogą zawierać znaki .,?!, znaki nowej linii \n i spacje na początku i końcu tekstu.
 
 # Input example 1:
 # Wlazł kotek na płotek i mruga. Ładna to piosenka, niedługa
